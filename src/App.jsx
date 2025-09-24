681122d751527c0935bd12655cb6c9ffbb4789b4
MRCOM <mrcom@MRCOMs-MacBook-Pro.local>
2025-09-24 21:40:22 +0500

    phala jasa

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import SearchBar from "./components/SearchBar";
 import WeatherCard from "./components/WeatherCard";
 import HourlyForecast from "./components/HourlyForecast";
 import DailyForecast from "./components/DailyForecast";
-import Weather from "./components/Weather";
 
 const API_KEY = "bc60ad1d0a3db0b5e76a60221e985185";
 
@@ -31,9 +30,7 @@ function App() {
   return (
     <div className="min-h-screen bg-[#0a0e27] text-white flex flex-col items-center p-6">
       <h1 className="text-3xl font-bold mb-6">How’s the sky looking today?</h1>
-       <div className="flex items-center justify-center min-h-screen bg-gray-900">
-      <WeatherCard />
-    </div>
+      
       <SearchBar city={city} setCity={setCity} onSearch={getWeather} />
 
       {weather && <WeatherCard weather={weather} />}
